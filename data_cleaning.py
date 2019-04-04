b5ea4c3c3f7cf794d4d26939c8e63c6ef85c53e0
jonas@valfridsson.net <jonas@valfridsson.net>
2019-04-04 19:06:42 +0200

    breathe, =, breathe

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 
+
 def str2float(x: "unknown"):
     if x == "nan":
         return 0
@@ -13,20 +14,23 @@ def str2float(x: "unknown"):
     else:
         return float(x)
 
+
 def nordpool_energy(dataframe: pd.DataFrame, out: str):
     swedish_energy_data = pd.DataFrame()
 
+    print(dataframe.columns.values)
     swedish_energy_data["price"] = dataframe["SE4"].apply(str2float)
     swedish_energy_data["date"] = dataframe.index.values
-    swedish_energy_data["hour"] = dataframe["Hours"].apply(lambda x: x[:3].strip())
+    swedish_energy_data["hour"] = dataframe["Hours"].apply(
+        lambda x: x[:3].strip())
     swedish_energy_data.fillna(0)
-    print(list(swedish_energy_data["price"]))
     swedish_energy_data.to_csv(out, index=False)
-
-    
-
 
 
 if __name__ == "__main__":
-    nordpool_energy(pd.read_csv("data/nordpool2018.csv"), "data/nordpool2018_clean.csv")
-
+    nordpool_energy(
+        pd.read_csv("data/nordpool2018.csv"), "data/nordpool2018_clean.csv")
+    nordpool_energy(
+        pd.read_csv("data/nordpool2017.csv"), "data/nordpool2017_clean.csv")
+    nordpool_energy(
+        pd.read_csv("data/nordpool2016.csv"), "data/nordpool2016_clean.csv")
