aa58636c6ff26a38b8d17f36efd5b5b0d82e5fd5
jonas@valfridsson.net <jonas@valfridsson.net>
2019-04-04 20:38:58 +0200

    more prizes

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import re
 
 
 def str2float(x: "unknown"):
@@ -18,11 +19,16 @@ def str2float(x: "unknown"):
 def nordpool_energy(dataframe: pd.DataFrame, out: str):
     swedish_energy_data = pd.DataFrame()
 
-    print(dataframe.columns.values)
     swedish_energy_data["price"] = dataframe["SE4"].apply(str2float)
-    swedish_energy_data["date"] = dataframe.index.values
+    swedish_energy_data["date"] = dataframe["Unnamed: 0"]
     swedish_energy_data["hour"] = dataframe["Hours"].apply(
         lambda x: x[:3].strip())
+    swedish_energy_data["day"] = dataframe["Unnamed: 0"].apply(
+        lambda x: re.findall("(..)-*", x)[0])
+    swedish_energy_data["month"] = dataframe["Unnamed: 0"].apply(
+        lambda x: re.findall("..-(..)-*", x)[0])
+    swedish_energy_data["year"] = dataframe["Unnamed: 0"].apply(
+        lambda x: re.findall("..-..-(....)", x)[0])
     swedish_energy_data.fillna(0)
     swedish_energy_data.to_csv(out, index=False)
 
