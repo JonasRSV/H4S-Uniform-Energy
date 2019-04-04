7b7232936242c932384f5bf0150d4c74ff86ff31
jonas@valfridsson.net <jonas@valfridsson.net>
2019-04-04 18:51:07 +0200

    Notebook

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,7 +2,11 @@ import numpy as np
 import pandas as pd
 
 def str2float(x: "unknown"):
-    if type(x) == str:
+    if x == "nan":
+        return 0
+    elif x != x:
+        return 0
+    elif type(x) == str:
         return float(x.replace(",", "."))
     elif type(x) == float:
         return x
@@ -15,6 +19,8 @@ def nordpool_energy(dataframe: pd.DataFrame, out: str):
     swedish_energy_data["price"] = dataframe["SE4"].apply(str2float)
     swedish_energy_data["date"] = dataframe.index.values
     swedish_energy_data["hour"] = dataframe["Hours"].apply(lambda x: x[:3].strip())
+    swedish_energy_data.fillna(0)
+    print(list(swedish_energy_data["price"]))
     swedish_energy_data.to_csv(out, index=False)
 
     
