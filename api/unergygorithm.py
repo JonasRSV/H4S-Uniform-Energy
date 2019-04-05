a8d3ca9afbfa7779555aa9d4694f00660b9718d9
jonas@valfridsson.net <jonas@valfridsson.net>
2019-04-05 17:41:00 +0200

    Apparently works-for-me is a crappy excuse.

--- api/unergygorithm.py
+++ api/unergygorithm.py
@@ -6,36 +6,36 @@ import pandas as pd
 from IPython import display
 
 
-def unergyp(day: np.ndarray, user_base) -> float:
-
-    daypdf = day / np.sum(np.abs(day))
-    dpmax = np.max(daypdf)
-    invdaypdf = (dpmax - daypdf) / np.sum(np.abs(dpmax - daypdf))
-
-    window = int(user_base * len(day)) // 2
-    invdaypdf = [
-        np.mean(invdaypdf[max(i - window, 0):min(i + window, len(day))])
-        for i in range(len(day))
-    ]
-
-    target = np.random.rand()
-
-    point = np.argmin(np.abs(np.cumsum(invdaypdf) - target))
-
-    return point
-
-
 def pdfs(day: np.ndarray, user_base: float) -> np.ndarray:
     daypdf = day / np.sum(np.abs(day))
     dpmax = np.max(daypdf)
 
     invdaypdf = (dpmax - daypdf) / np.sum(np.abs(dpmax - daypdf))
 
-    normalizer = np.exp( int(user_base * len(day)) // 2 
+    int(user_base * len(day))
+    daysize = len(day)
+
+    print(user_base)
+
+    std = np.exp(user_base)
+    # if 0.5 > user_base >= 0.25:
+        # std = 1.2
+    # elif 0.75 > user_base >= 0.5:
+        # std = 2
+    # elif user_base >= 0.75:
+        # std = 100 
+
+
+    normalizer = lambda mean: (lambda x: \
+            np.exp(-np.square(x - mean)/(2*std*std))/(std*np.sqrt(2*np.pi)))
+
     invdaypdf = np.array([
-        np.mean(invdaypdf[max(i - window, 0):min(i + window, len(day))])
+        np.sum(
+            np.array(list(map(normalizer(i), np.arange(daysize)))) * invdaypdf)
         for i in range(len(day))
     ])
+
+    invdaypdf = invdaypdf / np.sum(invdaypdf)
     return invdaypdf, daypdf
 
 
@@ -58,8 +58,8 @@ def demo(day: iter, fig, ax, axpdfs, speed=0.1, ipython=True):
         sb.lineplot(np.arange(unergypdf.shape[0]), unergypdf, ax=axpdfs)
         sb.lineplot(np.arange(daypdf.shape[0]), daypdf, ax=axpdfs)
 
-        sb.lineplot(np.arange(avg.shape[0]), avg, ax=ax)
-        plt.ylim([0, 0.2])
+        sb.barplot(np.arange(avg.shape[0]), avg, ax=ax)
+        plt.ylim([0, 0.3])
 
         if ipython:
             display.display(fig)
