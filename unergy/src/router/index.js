c8a72ad41c3e5653503160309e785925c6b15dc8
eleijonmarck <eric.leijonmarck@gmail.com>
2019-04-05 08:23:47 +0200

    routering

--- unergy/src/router/index.js
+++ unergy/src/router/index.js
@@ -10,12 +10,12 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    },
-    {
-      path: 'challenge/',
-      name: 'challenge',
-      component: Challenge
-    },
+    }
+    // {
+    //   path: 'challenge/',
+    //   name: 'challenge',
+    //   component: Challenge
+    // },
 
   ]
 })
